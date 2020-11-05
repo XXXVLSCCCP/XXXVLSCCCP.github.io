803d6be7fa77c5c6153708814377f6b806b03fba
Сергей <59625622+XXXVLSCCCP@users.noreply.github.com>
2020-11-05 21:41:10 +0300

    Add files via upload

---
+++ MyProectPurse/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="/style/style_slider.css">
+     
+</head>
+<body>
+<header class="header">
+   <div class="preloader">
+       <div class="loader"></div>
+   </div>
+   <div class="name_title">
+       <h1 >Коженные кошельки<br>ручной работы</h1>
+   </div>
+   <nav><a href="" class="header_botton">КУПИТЬ КОШЕЛЕК</a></nav>
+    <video loop="" muted="" autoplay="" poster="Video1" class="header__video">
+        <source src="video/video1.mp4" type="video/mp4">
+    </video>
+</header>
+<main>
+ <div class="top-gerb"></div>   
+<figure class="block-gerb"></figure>
+<div class="block-slider">
+<?php include("view/slider.php"); ?>
+</div>
+<div class="parallax-div">
+<div class="parallax"></div>
+<div class="parallax-opacity"></div>
+
+</div>
+<div class="block-bottom"></div>
+</main>
+<footer class="footer">
+
+</footer>
+    
+</body>
+</html>
---
+++ MyProectPurse/view/slider.php
@@ -0,0 +1,50 @@
+<div class="container">
+	
+
+	<input type="radio" id="i1" name="images" checked/>
+	<input type="radio" id="i2" name="images" />
+	<input type="radio" id="i3" name="images" />
+	<input type="radio" id="i4" name="images"  />
+	
+	<div class="slide_img" id="one">			
+			
+			<img src="img/foto_product/foto1.jpg">
+			
+				<label class="prev" for="i4"><span></span></label>
+				<label class="next" for="i2"><span></span></label>	
+		
+	</div>
+	
+	<div class="slide_img" id="two">
+		
+			<img src="img/foto_product/foto2.jpg " >
+			
+				<label class="prev" for="i1"><span></span></label>
+				<label class="next" for="i3"><span></span></label>
+		
+	</div>
+			
+	<div class="slide_img" id="three">
+			<img src="img/foto_product/foto3.jpg">	
+			
+				<label class="prev" for="i2"><span></span></label>
+				<label class="next" for="i4"><span></span></label>
+	</div>
+
+
+	<div class="slide_img" id="four">
+			<img src="/img/foto_product/foto4.jpg">	
+			
+				<label class="prev" for="i3"><span></span></label>
+				<label class="next" for="i1"><span></span></label>
+
+	</div>
+
+	<div id="nav_slide">
+		<label for="i1" class="dots" id="dot1"></label>
+		<label for="i2" class="dots" id="dot2"></label>
+		<label for="i3" class="dots" id="dot3"></label>
+		<label for="i4" class="dots" id="dot4"></label>
+	</div>
+		
+</div>
